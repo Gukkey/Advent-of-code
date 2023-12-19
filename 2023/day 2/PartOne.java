d55d52470155221c826f287eeed38517acc41c54
Gukkey <gukkey@protonmail.com>
2023-12-19 18:04:17 +0530

    added (partial) solution for day 2 part 1

--- 2023/day 2/PartOne.java
+++ 2023/day 2/PartOne.java
@@ -1,10 +1,31 @@
 import java.util.*;
+import java.util.stream.Stream;
 
 public class PartOne {
     public static void main(String[] args) {
+        int id = 1;
+        int sum = 0;
         Scanner sc = new Scanner(System.in);
-        while(sc.hasNext()){
-            
+        while (sc.hasNext()) {
+            id++;
+            if (checkConfiguration(sc.nextLine())) {
+                sum += id;
+            }
         }
+        System.out.println(sum);
+    }
+
+    public static boolean checkConfiguration(String s) {
+        final int red = 12;
+        final int green = 13;
+        final int blue = 14;
+        String[] cubeSet = s.split("\\s*;\\s*");
+        for (int i = 0; i < cubeSet.length; i++) {
+            //String[] cube = cubeSet[i].split("\\s*,\\s*");
+            cubeSet[i].lines().flatMap(x -> Arrays.stream(x.split("\\s*,\\s*"))).forEach(y -> {
+                
+            });
+        }
+        return false;
     }
 }
