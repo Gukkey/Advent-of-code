980593e1df6f14339c22e3a7fb4d63444542511e
Gukkey <gukkey@protonmail.com>
2023-12-20 14:05:47 +0530

    Added solution for Day 2 - Part One

--- 2023/day 2/PartOne.java
+++ 2023/day 2/PartOne.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Stream;
 
 public class PartOne {
@@ -6,11 +7,11 @@ public class PartOne {
         int id = 1;
         int sum = 0;
         Scanner sc = new Scanner(System.in);
-        while (sc.hasNext()) {
-            id++;
-            if (checkConfiguration(sc.nextLine())) {
+        while (sc.hasNextLine()) {
+            if (checkConfiguration(sc.nextLine().replaceAll("^.*:", "").trim())) {
                 sum += id;
             }
+            id++;
         }
         System.out.println(sum);
     }
@@ -19,13 +20,18 @@ public class PartOne {
         final int red = 12;
         final int green = 13;
         final int blue = 14;
+        AtomicBoolean flag = new AtomicBoolean(true);
         String[] cubeSet = s.split("\\s*;\\s*");
         for (int i = 0; i < cubeSet.length; i++) {
             //String[] cube = cubeSet[i].split("\\s*,\\s*");
             cubeSet[i].lines().flatMap(x -> Arrays.stream(x.split("\\s*,\\s*"))).forEach(y -> {
-                
+                if(y.contains("blue") && Integer.parseInt(y.replaceAll("[^\\d]", "")) > blue
+                    || y.contains("green") && Integer.parseInt(y.replaceAll("[^\\d]", "")) > green
+                    || y.contains("red") && Integer.parseInt(y.replaceAll("[^\\d]", "")) > red){
+                        flag.set(false);
+                }
             });
         }
-        return false;
+        return flag.get();
     }
 }
